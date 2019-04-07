74aeaae7bc66edc4524ba15c46b9b3a2377ddb82
Andreas Kessel <andreas.kessel@formatsoft.de>
2019-04-07 18:35:29 +0200

    Extension only suitable for TYPO3 >= 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'Tools for your TYPO3 installation',
 	'description' => 'Currently only one tool: regularly checks the size of the TYPO3 database. On exceeding a certain size, a mail is sent. Please, ask for free support in TYPO3 mailing lists or contact the maintainer for paid support.',
 	'category' => 'be',
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'author' => 'Andreas Kessel',
 	'author_email' => 'typo3-dev@formatsoft.de',
 	'author_company' => 'format Software Gmbh (www.formatsoft.de)',
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'clearCacheOnLoad' => 0,
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-9.5.99',
+			'typo3' => '9.5.0-9.5.99',
             'scheduler' => ''
 		],
         'conflicts' => [],
